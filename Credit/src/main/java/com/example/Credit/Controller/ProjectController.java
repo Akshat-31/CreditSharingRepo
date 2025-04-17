565a8ef740b749baa192b7b5be12a97f5edb340a
Akshat-31 <80938230+Akshat-31@users.noreply.github.com>
2025-04-17 15:07:12 -0400

    -Uploading service class and controller class (#2)
    Co-authored-by: akshatsehgal <akshatsehgal@192.168.2.122>

--- Credit/src/main/java/com/example/Credit/Controller/ProjectController.java
+++ Credit/src/main/java/com/example/Credit/Controller/ProjectController.java
@@ -1,8 +1,59 @@
 package com.example.Credit.Controller;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.Credit.Service.UserService;
+import com.example.Credit.model.CreditCard;
+import com.example.Credit.model.Offer;
+import com.example.Credit.model.User;
 
 @Controller
 public class ProjectController {
 
+	@Autowired
+	UserService userSerive;
+	
+	
+	@GetMapping("/home")
+	public String getHome() {
+		return "It's Working";
+	}
+	
+	@GetMapping("/getUserById")
+	public User getUserById(@RequestParam("id") long id) {
+		return userSerive.fetchUserFromId(id);
+	}
+	
+	@GetMapping("/getUsers")
+	public List<User> getAllUsers() {
+		return userSerive.fetchAllUser();
+	}
+	
+	@PostMapping("/addUser")
+	public boolean addUser(@RequestBody User user) {
+		return userSerive.addUser(user);
+	}
+	
+	@DeleteMapping("/deleteUser")
+	public void deleteUser(@RequestParam long id) {
+		userSerive.removeUserById(id);
+	}
+	
+	@GetMapping("/getAllOffers")
+	public List<Offer> getAllOffers() {
+		return userSerive.fetchAllOffers();
+	}
+	
+	@GetMapping("/getAllCreditCards")
+	public List<CreditCard> getAllCreditCards() {
+		return userSerive.fetchAllCreditCards();
+	}
 }
